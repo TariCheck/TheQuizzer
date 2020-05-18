71f6ea9b9827fab3d2d0d4bb7df9e2b05f0ba83b
TariCheck <65532878+TariCheck@users.noreply.github.com>
2020-05-18 11:48:04 +0200

    Les fichiers du quizz ! The quizz files !

---
+++ TheQuizzer/js/app.js
@@ -0,0 +1,20 @@
+var string = [""];
+
+var selection = prompt ("")
+
+
+if (string.includes(selection)) {
+    alert ('yes');
+} else {
+    alert ('no');
+};
+
+var string = [""]
+
+var selection = prompt ("")
+
+if (string.includes(selection)) {
+    alert ('yes');
+} else {
+    alert ('no');
+};
